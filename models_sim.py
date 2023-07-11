f138634beb23505983e49136eb94c91fe1641117
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-07-11 14:59:19 +0200

    Debug img size

--- models_sim.py
+++ models_sim.py
@@ -487,7 +487,7 @@ class SiameseIMViT(nn.Module):
 
 def sim_vit_base_patch16_dec512d8b(**kwargs):
     model = SiameseIMViT(
-        patch_size=16, img_size=384, embed_dim=768, depth=12, num_heads=12,
+        patch_size=16, img_size=352, embed_dim=768, depth=12, num_heads=12,
         decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
         mlp_ratio=4, norm_layer=partial(LayerNorm, eps=1e-6), **kwargs)
     return model
