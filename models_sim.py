2b8445b74830b9184c32e7d5a0ff33ba26f2c63d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-07-25 13:28:11 +0200

    extract features

--- models_sim.py
+++ models_sim.py
@@ -502,8 +502,8 @@ class SiameseIMViT(nn.Module):
 
         pred = pred.reshape(-1, pred.shape[-1])
         target = target.reshape(-1, target.shape[-1])
-        pred = torch.cat((pred, pred_boxes_features), dim=1)
-        target = torch.cat((target, target_boxes_features), dim=1)
+        pred = torch.cat((pred, pred_boxes_features), dim=0)
+        target = torch.cat((target, target_boxes_features), dim=0)
 
         # compute loss
         outputs = {}
