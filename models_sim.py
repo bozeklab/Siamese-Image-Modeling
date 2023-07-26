6c3d7d9f1f2644bba8ceb9bfc3f0dc53c5ad3f4c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-07-26 10:12:01 +0200

    debug

--- models_sim.py
+++ models_sim.py
@@ -493,12 +493,11 @@ class SiameseIMViT(nn.Module):
 
         print('dupa')
 
-        pred_boxes_features = self.extract_box_feature(x=pred, boxes_info=boxes1, scale_factor=1. / self.patch_size,
-                                                       mask=mask)
-        target_boxes_features = self.extract_box_feature(x=target, boxes_info=boxes2, scale_factor=1. / self.patch_size,
-                                                         mask=mask)
-
-        print('dupa2')
+        with torch.no_grad():
+            pred_boxes_features = self.extract_box_feature(x=pred, boxes_info=boxes1, scale_factor=1. / self.patch_size,
+                                                           mask=mask)
+            target_boxes_features = self.extract_box_feature(x=target, boxes_info=boxes2, scale_factor=1. / self.patch_size,
+                                                             mask=mask)
 
         pred_boxes_features = self.box_embed(pred_boxes_features).squeeze()
         target_boxes_features = self.box_embed(target_boxes_features).squeeze()
