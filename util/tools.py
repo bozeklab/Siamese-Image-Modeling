264cda4e42cd5fefce64cad7206396cfdcdc4424
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-08-11 15:34:01 +0200

    add cpu

--- util/tools.py
+++ util/tools.py
@@ -112,7 +112,7 @@ def calculate_step_metric(predictions, gt, num_nuclei_classes, magnification=40)
     predictions["instance_types_nuclei"] = (
         predictions["instance_types_nuclei"].detach().cpu().numpy().astype("int32")
     )
-    instance_maps_gt = gt["instance_map"]
+    instance_maps_gt = gt["instance_map"].detach().cpu()
     gt["tissue_types"] = gt["tissue_types"].detach().cpu().numpy().astype(np.uint8)
     gt["nuclei_binary_map"] = torch.argmax(gt["nuclei_binary_map"], dim=-1).type(
         torch.uint8
