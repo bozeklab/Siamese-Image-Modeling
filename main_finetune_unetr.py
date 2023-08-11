c07b5f9be464110d4803ddaac9c3fc806d69e622
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-08-11 16:09:57 +0200

    finetune code

--- main_finetune_unetr.py
+++ main_finetune_unetr.py
@@ -253,6 +253,8 @@ def main(args):
             prob=args.mixup_prob, switch_prob=args.mixup_switch_prob, mode=args.mixup_mode,
             label_smoothing=args.smoothing, num_classes=args.nb_classes)
 
+    print('!!!!!!!!!!!!')
+
     # build model
     model = models_vit.__dict__[args.model](
         num_classes=args.nb_classes,
