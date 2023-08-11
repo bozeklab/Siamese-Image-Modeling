84444b623b9dca495434a406c16b666fa8187078
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-08-11 16:37:09 +0200

    finetune code

--- main_finetune_unetr.py
+++ main_finetune_unetr.py
@@ -56,6 +56,8 @@ def get_args_parser():
     parser.add_argument('--model', default='vit_large_patch16', type=str, metavar='MODEL',
                         help='Name of model to train')
 
+    parser.add_argument('--embed_dim', default=768, type=int)
+
     parser.add_argument('--input_size', default=224, type=int,
                         help='images input size')
 
