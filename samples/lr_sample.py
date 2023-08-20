e233fb890b3ec1a9f33b7fae55cbe99398b4d77f
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-08-20 13:53:42 +0200

    exp learning schedule

--- samples/lr_sample.py
+++ samples/lr_sample.py
@@ -1,5 +1,12 @@
 import matplotlib.pyplot as plt
 import math
+import torch
+
+class Args:
+    lr = 0.0003
+    min_lr = 1e-4
+    warmup_epochs = 5
+    epochs = 130
 
 def adjust_learning_rate(optimizer, epoch, args):
     """Decay the learning rate with half-cycle cosine after warmup"""
@@ -10,36 +17,40 @@ def adjust_learning_rate(optimizer, epoch, args):
             (1. + math.cos(math.pi * (epoch - args.warmup_epochs) / (args.epochs - args.warmup_epochs)))
     return lr
 
-# Define the parameters
-class Args:
-    lr = 0.0003
-    min_lr = 1e-4
-    warmup_epochs = 5
-    epochs = 130
-
+def exponential_lr_schedule(epoch, args):
+    base_lr = args.lr
+    gamma = 0.85
+    lr = base_lr * math.pow(gamma, epoch)
+    return lr
 
 def main():
     args = Args()
 
     # Create lists to store epoch and learning rate values
     epochs = list(range(args.epochs))
-    learning_rates = []
+    learning_rates_adjusted = []
+    learning_rates_exponential = []
 
-    # Calculate and store learning rates for each epoch
+    # Calculate and store learning rates for each epoch using adjusted method
     for epoch in epochs:
-        lr = adjust_learning_rate(None, epoch, args)
-        learning_rates.append(lr)
+        lr_adjusted = adjust_learning_rate(None, epoch, args)
+        learning_rates_adjusted.append(lr_adjusted)
+
+    # Calculate learning rates for each epoch using exponential schedule
+    for epoch in epochs:
+        lr_exponential = exponential_lr_schedule(epoch, args)
+        learning_rates_exponential.append(lr_exponential)
 
     # Plot the learning rate chart
-    plt.plot(epochs, learning_rates)
+    plt.figure(figsize=(10, 6))
+    plt.plot(epochs, learning_rates_adjusted, label='Adjusted LR Schedule')
+    plt.plot(epochs, learning_rates_exponential, label='Exponential LR Schedule')
     plt.xlabel('Epoch')
     plt.ylabel('Learning Rate')
-    plt.title('Learning Rate Schedule')
+    plt.title('Learning Rate Schedules')
     plt.grid(True)
+    plt.legend()
     plt.show()
 
 if __name__ == "__main__":
     main()
-
-
-
--- util/lr_sched.py
+++ util/lr_sched.py
@@ -9,6 +9,7 @@
 
 import math
 
+
 def adjust_learning_rate(optimizer, epoch, args):
     """Decay the learning rate with half-cycle cosine after warmup"""
     if epoch < args.warmup_epochs:
@@ -16,6 +17,18 @@ def adjust_learning_rate(optimizer, epoch, args):
     else:
         lr = args.min_lr + (args.lr - args.min_lr) * 0.5 * \
             (1. + math.cos(math.pi * (epoch - args.warmup_epochs) / (args.epochs - args.warmup_epochs)))
+    for param_group in optimizer.param_groups:
+        if "lr_scale" in param_group:
+            param_group["lr"] = lr * param_group["lr_scale"]
+        else:
+            param_group["lr"] = lr
+    return lr
+
+
+def adjust_exp_learning_rate(optimizer, epoch, args):
+    base_lr = args.lr
+    gamma = 0.85
+    lr = base_lr * math.pow(gamma, epoch)
     for param_group in optimizer.param_groups:
         if "lr_scale" in param_group:
             param_group["lr"] = lr * param_group["lr_scale"]
