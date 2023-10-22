a950a7bfb98c5270143afbd2ae90c102f54dae9d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-10-22 20:00:12 +0200

    attn dump

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -90,7 +90,8 @@ def train_one_epoch(model: torch.nn.Module,
                 loss, outputs, attn = model(x1, x2, boxes2, rel_pos_21, mm, update_mm, mask=mask)
                 metric_logger.update(**outputs)
 
-            attentions = attentions[0, :, 0, 1:]
+            attentions = attn[0, :, 0, 1:]
+            del attn
             vattn = visualize_attention(attentions, 14, 14,
                                         patch_size=16, threshold=0.6)
 
