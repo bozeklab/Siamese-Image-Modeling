7608337799d11b472e938bc673dcfc17b54e4512
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-10-22 20:55:36 +0200

    attn dump

--- models_sim.py
+++ models_sim.py
@@ -474,7 +474,7 @@ class SiameseIMViT(nn.Module):
         print('!!!')
         print(self.last_attn[len(self.predictor_decoder_blocks) - 1].shape)
 
-        attn = self.last_attn[len(self.predictor_decoder_blocks) - 1][:, 0, -x2_embed.shape[1]:, -x2_embed.shape[1]:]
+        attn = self.last_attn[len(self.predictor_decoder_blocks) - 1][:, :, 0, -x2_embed.shape[1]:]
         print('!!!!')
         print(attn.shape)
 
