14b8eae0cbff209a233bd1f01ce9ccf5bd446447
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-10-22 21:16:09 +0200

    attn dump

--- util/tools.py
+++ util/tools.py
@@ -37,7 +37,8 @@ def visualize_attention(attentions, w_featmap, h_featmap, patch_size=16, thresho
 
     attentions = attentions.reshape(nh, w_featmap, h_featmap)
     attentions = nn.functional.interpolate(attentions.unsqueeze(0), scale_factor=patch_size, mode="nearest")[
-        0].cpu()
+        0].cpu().numpy()
+    print(attentions)
     return plt.get_cmap('viridis', attentions)
 
 
