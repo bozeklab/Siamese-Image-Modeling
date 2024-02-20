9fd46180c78c206b4af230841f8f09e02d187c1b
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-02-20 17:22:41 +0100

    samples

--- main_pretrain.py
+++ main_pretrain.py
@@ -462,8 +462,8 @@ class DataAugmentationForSIM(object):
 
         self.format_transform = transforms.Compose([
             transforms.ToTensor(),
-            #transforms.Normalize(
-            #    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
     def __call__(self, image):
